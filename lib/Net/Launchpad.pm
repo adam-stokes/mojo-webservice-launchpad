b09c6f082e3cf78c079cd09be817c0fa577cf39f
Adam Stokes <adam.stokes@ubuntu.com>
2014-01-10 12:28:40 -0500

    initial
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ lib/Net/Launchpad.pm
@@ -0,0 +1,207 @@
+package Net::Launchpad;
+
+use Mojo::Base -base;
+use Mojo::UserAgent;
+
+use Net::OAuth;
+$Net::OAuth::PROTOCOL_VERSION = Net::OAuth::PROTOCOL_VERSION_1_0;
+
+our $VERSION = '0.99_1';
+
+has 'ua' => sub { my $self = shift; Mojo::UserAgent->new };
+has staging => 0;
+has 'consumer_key';
+has 'access_token';
+has 'access_token_secret';
+
+sub request_token_url {
+    my $self = shift;
+    if ($self->staging) {
+        'https://staging.launchpad.net/+request-token';
+    }
+    else {
+        'https://launchpad.net/+request-token';
+    }
+}
+
+sub access_token_url {
+    my $self = shift;
+    if ($self->staging) {
+        'https://staging.launchpad.net/+access-token';
+    }
+    else {
+        'https://launchpad.net/+access-token';
+    }
+}
+
+sub authorize_token_url {
+    my $self = shift;
+    if ($self->staging) {
+        'https://staging.launchpad.net/+authorize-token';
+    }
+    else {
+        'https://launchpad.net/+authorize-token';
+    }
+}
+
+sub api_url {
+    my $self = shift;
+    if ($self->staging) {
+        'https://api.staging.launchpad.net/1.0';
+    }
+    else {
+        'https://api.launchpad.net/1.0';
+    }
+}
+
+sub _nonce {
+    my $self  = shift;
+    my @a     = ('A' .. 'Z', 'a' .. 'z', 0 .. 9);
+    my $nonce = '';
+    for (0 .. 31) {
+        $nonce .= $a[rand(scalar(@a))];
+    }
+    return $nonce;
+}
+
+sub login_with_creds {
+    my $self    = shift;
+    my $request = Net::OAuth->request('consumer')->new(
+        consumer_key     => $self->consumer_key,
+        consumer_secret  => '',
+        request_url      => $self->request_token_url,
+        request_method   => 'POST',
+        signature_method => 'PLAINTEXT',
+        timestamp        => time,
+        nonce            => $self->_nonce,
+    );
+
+    $request->sign;
+    my $res =
+      $self->ua->post($request->to_url => $request->to_post_body );
+    die "Failed to get response: ". $res->res->message unless $res->res->code == 200;
+    my $response =
+      Net::OAuth->response('request token')->from_post_body($res->res->body);
+    my $_token        = $response->token;
+    my $_token_secret = $response->token_secret;
+    say
+      "Go here in your browser, hit [ENTER] once you've approved on launchpad.net";
+    say $self->authorize_token_url . "?oauth_token=" . $_token;
+    <STDIN>;
+
+    $request = Net::OAuth->request('access token')->new(
+        consumer_key     => $self->consumer_key,
+        consumer_secret  => '',
+        token            => $_token,
+        token_secret     => $_token_secret,
+        request_url      => $self->access_token_url,
+        request_method   => 'POST',
+        signature_method => 'PLAINTEXT',
+        timestamp        => time,
+        nonce            => $self->_nonce
+    );
+
+    $request->sign;
+    $res = $self->ua->post($request->to_url => $request->to_post_body);
+    die "Failed to get response" unless $res->res->code == 200;
+    $response =
+      Net::OAuth->response('access token')->from_post_body($res->res->body);
+    $self->access_token($response->token);
+    $self->access_token_secret($response->token_secret);
+    return;
+}
+
+1;
+
+=head1 NAME
+
+Net::Launchpad - Launchpad.net OAuth 1.0
+
+=head1 SYNOPSIS
+
+OAuth 1.0a authorization and client for Launchpad.net
+
+=head1 ATTRIBUTES
+
+L<Net::Launchpad> implements the following attributes:
+
+=head2 B<staging>
+
+Boolean to interact with staging server or production.
+
+=head2 B<ua>
+
+A L<Mojo::UserAgent>.
+
+=head2 B<consumer_key>
+
+Holds the string that identifies your application.
+
+    $lp->consumer_key('my-app-name');
+
+=head2 B<access_token>
+
+Token received from authorized request
+
+=head2 B<access_token_secret>
+
+Token secret received from authorized request
+
+=head1 METHODS
+
+=head2 B<access_token_url>
+
+OAuth Access token url
+
+=head2 B<authorize_token_url>
+
+OAuth Authorize token url
+
+=head2 B<request_token_url>
+
+OAuth Request token url
+
+=head2 B<api_url>
+
+API url for doing the client interactions with launchpad.net
+
+=head1 AUTHOR
+
+Adam Stokes, C<< <adamjs at cpan.org> >>
+
+=head1 BUGS
+
+Report bugs to https://github.com/battlemidget/Net-Launchpad/issues.
+
+=head1 DEVELOPMENT
+
+=head2 Repository
+
+    http://github.com/battlemidget/Net-Launchpad
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::Launchpad
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<https://launchpad.net/launchpadlib>, "Python implementation"
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright 2013-2014 Adam Stokes
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+
