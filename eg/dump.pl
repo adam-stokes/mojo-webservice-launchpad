f94da5b973a35e5ced869d9f5cd7106bc08ad82f
Adam Stokes <adam.stokes@ubuntu.com>
2014-11-05 16:44:01 -0500

    add supervisor link resource
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ eg/dump.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+#
+# for quick tests only, should not be depended upon for
+# proper examples of current api.
+
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/../../lib";
+use Net::Launchpad::Client;
+use Net::Launchpad::Model;
+use List::AllUtils qw(first);
+use Data::Dumper::Concise;
+
+my $public_bug = $ENV{LP_BUG} || '1388929';
+
+my $lpc = Net::Launchpad::Client->new(
+    consumer_key        => $ENV{LP_CONSUMER_KEY},
+    access_token        => $ENV{LP_ACCESS_TOKEN},
+    access_token_secret => $ENV{LP_ACCESS_TOKEN_SECRET}
+);
+
+my $model = Net::Launchpad::Model->new(lpc => $lpc);
+
+my $bug = $model->bug($public_bug);
+my $tasks = $bug->tasks;
+
+my $branch = $model->branch('~adam-stokes', '+junk', 'cloud-installer');
+#print Dumper($branch->dependent_branches);
+
+my $project = $model->project('cloud-installer');
+# print Dumper($project->{result});
+
+#p $bug_item;
+# p $bug->tasks;
+# p $bug->date_created;
+# p $bug->watches;
+# my $bugtask =
+#   first { $_->{bug_target_name} =~ /ubuntu-advantage|(Ubuntu)/ } @{$bug->tasks};
+# # p $bugtask;
+# my $person = $c->model('Person')->by_name('~adam-stokes');
+# p $person;
+# p $person->ppas;
--- lib/Net/Launchpad/Role/Project.pm
+++ lib/Net/Launchpad/Role/Project.pm
@@ -7,4 +7,13 @@ use Function::Parameters;
 
 with 'Net::Launchpad::Role::Common';
 
+=method bug_supervisor_link
+
+Bug supervisor
+
+=cut
+
+method bug_supervisor {
+    return $self->resource('bug_supervisor');
+}
 1;
