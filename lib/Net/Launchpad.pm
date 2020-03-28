9e4b3c1590f7204b50d909f75608c3b26d6dd2f0
Adam Stokes <battlemidget@users.noreply.github.com>
2020-03-28 15:23:09 -0400

    tidy
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- lib/Net/Launchpad.pm
+++ lib/Net/Launchpad.pm
@@ -8,30 +8,27 @@ use Mojo::URL;
 use Mojo::Parameters;
 use Data::Dumper::Concise;
 
-our $VERSION = '0.01';
+our $VERSION   = '0.01';
 our $AUTHORITY = 'cpan:ADAMJS';
-
 
 has staging => 0;
 has consumer_key;
 has callback_uri;
 
 has ua => sub {
-        my $ua = Mojo::UserAgent->new;
-        $ua->transactor->name("perl-net-launchpad");
-        return $ua;
+    my $ua = Mojo::UserAgent->new;
+    $ua->transactor->name("perl-net-launchpad");
+    return $ua;
 };
 
-
 has nonce => sub {
-    my @a = ('A' .. 'Z', 'a' .. 'z', 0 .. 9);
+    my @a     = ( 'A' .. 'Z', 'a' .. 'z', 0 .. 9 );
     my $nonce = '';
-    for (0 .. 31) {
-        $nonce .= $a[rand(scalar(@a))];
+    for ( 0 .. 31 ) {
+        $nonce .= $a[ rand( scalar(@a) ) ];
     }
     return $nonce;
 };
-
 
 has params => sub {
     return {
@@ -45,7 +42,7 @@ has params => sub {
         oauth_timestamp        => time,
         oauth_nonce            => $self->nonce
     };
-}
+};
 
 sub api_host {
     return Mojo::URL->new('https://launchpad.net/') unless $self->staging;
@@ -67,37 +64,36 @@ sub authorize_token_path {
 sub request_token {
     my $tx =
       $self->ua->post(
-        $self->request_token_path->to_string => form => $self->params);
+        $self->request_token_path->to_string => form => $self->params );
     die $tx->res->body unless $tx->success;
-    my $params = Mojo::Parameters->new($tx->res->body);
-    my $token = $params->param('oauth_token');
+    my $params = Mojo::Parameters->new( $tx->res->body );
+    my $token  = $params->param('oauth_token');
     my $secret = $params->param('oauth_token_secret');
-    return ($token, $secret);
+    return ( $token, $secret );
 }
 
-sub authorize_token($token, $token_secret) {
-    $self->params->{oauth_token} = $token;
+sub authorize_token ( $token, $token_secret ) {
+    $self->params->{oauth_token}        = $token;
     $self->params->{oauth_token_secret} = $token_secret;
-    my $url = $self->authorize_token_path->query($self->params);
+    my $url = $self->authorize_token_path->query( $self->params );
     return $url->to_string;
 }
 
-sub access_token($token, $secret) {
-    $self->params->{oauth_token} = $token;
+sub access_token ( $token, $secret ) {
+    $self->params->{oauth_token}        = $token;
     $self->params->{oauth_token_secret} = $secret;
-    $self->params->{oauth_signature} =
-      '&' . $secret;
+    $self->params->{oauth_signature}    = '&' . $secret;
     my $tx =
       $self->ua->post(
-        $self->access_token_path->to_string => form => $self->params);
+        $self->access_token_path->to_string => form => $self->params );
     die $tx->res->body unless $tx->success;
-    my $params = Mojo::Parameters->new($tx->res->body);
+    my $params = Mojo::Parameters->new( $tx->res->body );
     print Dumper($params);
-    return ($params->param('oauth_token'), $params->param('oauth_token_secret'));
+    return ( $params->param('oauth_token'),
+        $params->param('oauth_token_secret') );
 }
 
 1;
-
 
 =head1 AUTHOR
 
--- lib/Net/Launchpad/Model.pm
+++ lib/Net/Launchpad/Model.pm
@@ -7,94 +7,85 @@ use Moose::Util qw(apply_all_roles is_role does_role search_class_by_role);
 use Module::Runtime qw(is_module_name use_package_optimistically);
 use Data::Dumper::Concise;
 
-sub _load_model ($name, $params) {
+sub _load_model ( $name, $params ) {
     my $model_class = "Net::Launchpad::Model::$name";
     my $model_role  = "Net::Launchpad::Role::$name";
 
     die "Invalid model requested." unless is_module_name($model_class);
-    die "Unknown Role module" unless is_module_name($model_role);
+    die "Unknown Role module"      unless is_module_name($model_role);
 
-    my $model = use_package_optimistically($model_class)
-        my $model = $model->with_roles($model_role)
-        return $model->new(result => $params);
+    my $model = use_package_optimistically($model_class);
+    my $model = $model->with_roles($model_role);
+    return $model->new( result => $params );
 }
 
-sub archive ($distro, $archive_name) {
+sub archive ( $distro, $archive_name ) {
     my $params = $self->get(
-        sprintf(
-            "%s/+archive/%s", $self->api_url, $distro, $archive_name
-        )
-    );
-    return $self->_load_model('Archive', $params);
+        sprintf( "%s/+archive/%s", $self->api_url, $distro, $archive_name ) );
+    return $self->_load_model( 'Archive', $params );
 }
 
 sub bug (Int $id) {
-    my $params =
-      $self->get(sprintf("%s/bugs/%s", $self->api_url, $id));
-    return $self->_load_model('Bug', $params);
+    my $params = $self->get( sprintf( "%s/bugs/%s", $self->api_url, $id ) );
+    return $self->_load_model( 'Bug', $params );
 }
 
 sub bugtracker ($name) {
-    my $params = $self->get(
-        sprintf("%s/bugs/bugtrackers/%s", $self->api_url, $name));
-    return $self->_load_model('BugTracker', $params);
+    my $params =
+      $self->get( sprintf( "%s/bugs/bugtrackers/%s", $self->api_url, $name ) );
+    return $self->_load_model( 'BugTracker', $params );
 }
 
 sub builder ($name) {
     my $params =
-      $self->get(sprintf("%s/builders/%s", $self->api_url, $name));
-    return $self->_load_model('Builder', $params);
+      $self->get( sprintf( "%s/builders/%s", $self->api_url, $name ) );
+    return $self->_load_model( 'Builder', $params );
 }
 
 sub country ($country_code) {
     my $params = $self->get(
-        sprintf("%s/+countries/%s", $self->api_url, $country_code));
-    return $self->_load_model('Country', $params);
+        sprintf( "%s/+countries/%s", $self->api_url, $country_code ) );
+    return $self->_load_model( 'Country', $params );
 }
 
-
-sub branch ($name, $project_name, $branch_name) {
+sub branch ( $name, $project_name, $branch_name ) {
     my $params = $self->get(
-        sprintf("%s/%s/%s/%s",
-            $self->api_url, $name, $project_name, $branch_name)
+        sprintf( "%s/%s/%s/%s",
+            $self->api_url, $name, $project_name, $branch_name )
     );
-    return $self->_load_model('Branch', $params);
+    return $self->_load_model( 'Branch', $params );
 }
 
 sub people {
-    my $params =
-      $self->get(sprintf("%s/people", $self->api_url));
-    return $self->_load_model('People', $params);
+    my $params = $self->get( sprintf( "%s/people", $self->api_url ) );
+    return $self->_load_model( 'People', $params );
 }
 
 sub person ($name) {
-    my $params =
-      $self->get(sprintf("%s/%s", $self->api_url, $name));
-    return $self->_load_model('Person', $params);
+    my $params = $self->get( sprintf( "%s/%s", $self->api_url, $name ) );
+    return $self->_load_model( 'Person', $params );
 }
 
 sub distribution ($name) {
-    my $params =
-      $self->get(sprintf("%s/%s", $self->api_url, $name));
-    return $self->_load_model('Distribution', $params);
+    my $params = $self->get( sprintf( "%s/%s", $self->api_url, $name ) );
+    return $self->_load_model( 'Distribution', $params );
 }
 
 sub language ($isocode) {
-    my $params = $self->get(
-        sprintf("%s/+languages/%s", $self->api_url, $isocode));
-    return $self->_load_model('Language', $params);
+    my $params =
+      $self->get( sprintf( "%s/+languages/%s", $self->api_url, $isocode ) );
+    return $self->_load_model( 'Language', $params );
 }
 
 sub cve ($cve) {
     my $params =
-      $self->get(sprintf("%s/bugs/cve/%s", $self->api_url, $cve));
-    return $self->_load_model('CVE', $params);
+      $self->get( sprintf( "%s/bugs/cve/%s", $self->api_url, $cve ) );
+    return $self->_load_model( 'CVE', $params );
 }
 
 sub project ($name) {
-    my $params =
-      $self->get(sprintf("%s/%s", $self->api_url, $name));
-    return $self->_load_model('Project', $params);
+    my $params = $self->get( sprintf( "%s/%s", $self->api_url, $name ) );
+    return $self->_load_model( 'Project', $params );
 }
 
 1;
